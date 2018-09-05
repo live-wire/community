02522d04158f84218e27c27061ef98b96c7a9792
mohinderps <mohin19derp@gmail.com>
2018-09-05 23:46:46 +0530

    Test Routing

---
+++ frontend/src/components/PasswordReset/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PasswordForget = () => (
+  <div>
+    Password Forget
+  </div>
+);
+
+export default PasswordForget;
---
+++ frontend/src/constants/routes.js
@@ -0,0 +1,6 @@
+export const SIGNUP = '/signup';
+export const LOGIN = '/login';
+export const LANDING = '/';
+export const HOME = '/home';
+export const ACCOUNT = '/account';
+export const PASSWORD_RESET = '/password-reset';
