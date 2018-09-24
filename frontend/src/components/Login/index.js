241e6183e9e6fc2354d3614e3a7f10ae4f88cb98
mohinderps <mohin19derp@gmail.com>
2018-09-24 23:53:00 +0530

    Decent Login form with semantic ui

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { Button, Input, Divider, Header } from 'semantic-ui-react';
+import { Button, Input, Header, Icon, Form } from 'semantic-ui-react';
 
 import * as routes from '../../constants/routes';
 import { isValidUser, isValidPassword } from '../../helpers/validations';
@@ -12,10 +12,13 @@ class Login extends Component {
 		this.state = {
 			user: '',
 			password: '',
-			error: null
+			showPassword: false,
+			error: null,
+			loading: false
 		};
 		this.handleInputChange = this.handleInputChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleToggleShowPassword = this.handleToggleShowPassword.bind(this);
 	}
 
 	handleInputChange(event) {
@@ -23,6 +26,12 @@ class Login extends Component {
 
 		this.setState({
 			[name]: value
+		});
+	}
+
+	handleToggleShowPassword() {
+		this.setState({
+			showPassword: !this.state.showPassword
 		});
 	}
 
@@ -35,21 +44,26 @@ class Login extends Component {
 	handleSubmit(event) {
 		event.preventDefault();
 		const { history } = this.props;
-
+		this.setState({
+			loading: true
+		});
 		fakeAuth
 			.login()
 			.then(res => {
-				history.push(routes.HOME);
+				//history.push(routes.HOME);
+				this.setState({
+					loading: false
+				});
 			})
 			.catch(err => {
 				this.setState({
-					error: err
+					loading: false
 				});
 			});
 	}
 
 	render() {
-		const { user, password, error } = this.state;
+		const { user, password, error, showPassword, loading } = this.state;
 		const isEnabled = this.canBeSubmitted();
 
 		return (
@@ -57,35 +71,60 @@ class Login extends Component {
 				<Header as="h2" textAlign="center">
 					Login to Community
 				</Header>
-				<form onSubmit={this.handleSubmit}>
-					{/* <input
-						name="user"
-						placeholder="Username or Email"
-						type="text"
-						value={user}
-						onChange={this.handleInputChange}
-						autoComplete="off"
-					/> */}
-					<Input focus fluid placeholder="Username or Email" />
-					<Divider hidden />
-					{/* <input
-						name="password"
-						placeholder="Password"
-						type="password"
-						value={password}
-						onChange={this.handleInputChange} />*/}
-					<Input fluid placeholder="Password" />
-					<Divider hidden />
-					{/* <div>
-						<button type="submit" disabled={!isEnabled}>
+				<Form onSubmit={this.handleSubmit}>
+					<Form.Field>
+						<Input
+							focus
+							fluid
+							type="text"
+							name="user"
+							placeholder="Username or Email"
+							value={user}
+							onChange={this.handleInputChange}
+							autoComplete="off"
+						/>
+					</Form.Field>
+					<Form.Field>
+						{showPassword ? (
+							<Input
+								fluid
+								type="text"
+								name="password"
+								placeholder="Password"
+								value={password}
+								onChange={this.handleInputChange}
+								icon={
+									<Icon
+										name="eye"
+										link
+										onClick={this.handleToggleShowPassword}
+									/>
+								}
+							/>
+						) : (
+							<Input
+								fluid
+								type="password"
+								name="password"
+								placeholder="Password"
+								value={password}
+								onChange={this.handleInputChange}
+								icon={
+									<Icon
+										name="eye slash"
+										link
+										onClick={this.handleToggleShowPassword}
+									/>
+								}
+							/>
+						)}
+					</Form.Field>
+					<Form.Field>
+						<Button fluid primary disabled={!isEnabled} loading={loading}>
 							Log In
-						</button>
-					</div> */}
-					<Button primary fluid disabled={!isEnabled}>
-						Login
-					</Button>
-					{/* <div>{error && <p>{error.message}</p>}</div> */}
-				</form>
+						</Button>
+					</Form.Field>
+				</Form>
 			</div>
 		);
 	}
