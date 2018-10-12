54a204df05ddfcea96764784b2ae232c3e7ff407
live-wire <dhruv.life@hotmail.com>
2018-10-12 02:47:33 +0200

    Updated pagination

--- unchained/community/community/settings.py
+++ unchained/community/community/settings.py
@@ -54,7 +54,7 @@ from .csrfsession import CsrfExemptSessionAuthentication
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 5,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication'
