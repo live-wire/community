439c050856906e78686cf1e0f0baa3b890ce8bab
live-wire <dhruv.life@hotmail.com>
2018-12-16 22:48:28 +0100

    Updated permission checks to accept token based authentications

--- unchained/community/community/permissions.py
+++ unchained/community/community/permissions.py
@@ -2,9 +2,37 @@ from rest_framework import permissions
 import pickle
 from django.core.cache import cache
 from functools import reduce
+from django.contrib.auth.models import User, AnonymousUser
+from rest_framework.authtoken.models import Token
+from rest_framework import authentication, exceptions
+
+def get_user(request):
+	auth = authentication.get_authorization_header(request)
+	if auth is not None:
+		auth = auth.decode("utf-8")
+		auth = auth.split(" ")
+	if auth is None or 'Token' not in auth:
+		return None
+	if len(auth) == 1:
+		msg = _('Invalid token header. No credentials provided.')
+		raise exceptions.AuthenticationFailed(msg)
+	elif len(auth) > 2:
+		msg = _('Invalid token header. Credentials string should not contain spaces.')
+		raise exceptions.AuthenticationFailed(msg)
+	try:
+		token = Token.objects.get(key=str(auth[1]))
+		user = token.user
+		return user
+	except Token.DoesNotExist:
+		raise exceptions.AuthenticationFailed('Token Invalid')
+	return None
+
 
 def localcache(f):
 	def wrapper(*args, **kwargs):
+		# Making sure that the user is not just some AnonymousUser
+		if type(args[0].user) is AnonymousUser:
+			args[0].user = get_user(args[0])
 		# First argument will always be request
 		key = ''
 		key += f.__name__ + '-' # function_name
@@ -58,7 +86,7 @@ def getUserCourses(request):
 
 @localcache
 def belongsToInstitution(request, institution):
-	if (request.user.is_superuser or 
+	if (request.user.is_superuser or
 	institution.students.filter(user_id=request.user.id).exists() or 
 	institution.teachers.filter(user_id=request.user.id).exists() or 
 	institution.administrators.filter(user_id=request.user.id).exists()):
