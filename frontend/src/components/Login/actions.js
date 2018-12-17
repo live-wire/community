318fcb4a540b15c6501c54a92bda148bf8340100
live-wire <dhruv.life@hotmail.com>
2018-12-17 02:07:14 +0100

    Trying front end dockerization

--- frontend/src/components/Login/actions.js
+++ frontend/src/components/Login/actions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const login = (username, password) => {
-	return axios.post('http://35.204.29.216:8000/auth/login', {
+	return axios.post('http://localhost:8000/auth/login', {
 		username,
 		password
 	}, {headers: {
--- frontend/src/components/Students/actions.js
+++ frontend/src/components/Students/actions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getStudents = token => {
-	return axios.get('http://35.204.29.216:8000/student/', {
+	return axios.get('http://localhost:8000/student/', {
 		headers: {
 			'Content-Type': 'application/json',
 			'Authorization': `Token ${token}`
