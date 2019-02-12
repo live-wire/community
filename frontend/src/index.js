7c9e5464015d4bdd0a6995c804886da7072fe749
mohinderps <mohin19derp@gmail.com>
2019-02-12 14:43:36 +0530

    theming

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import App from './components/App';
-import './globalStyle';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme/theme';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
-	<Router>
-		<App />
-	</Router>,
+	<ThemeProvider theme={theme}>
+		<React.Fragment>
+			<GlobalStyle />
+			<Router>
+				<App />
+			</Router>
+		</React.Fragment>
+	</ThemeProvider>,
 	document.getElementById('root')
 );
