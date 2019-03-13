b73f910d6ec008191941b9e5778d12ed3f2ed8ca
mohinderps <mohin19derp@gmail.com>
2019-03-13 23:35:13 +0530

    fixed header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Dropdown } from 'semantic-ui-react';
+import styled from 'styled-components';
 import Box from '../framework/components/basic/Box';
 import Flex from '../framework/components/basic/Flex';
+
+const StyledDiv = styled.div`
+	cursor: pointer;
+`;
 
 const Header = ({ logUserOut }) => (
 	<Box borderBottom boxShadow="rgba(37, 11, 54, 0.04) 0px 2px 0px">
 		<Flex.Container flexDirection="row" justifyContent="flex-end">
 			<Flex.Child>
-				<Dropdown text="Dhruv" direction="left">
-					<Dropdown.Menu>
-						<Dropdown.Item text="Sign Out" onClick={() => logUserOut()} />
-					</Dropdown.Menu>
-				</Dropdown>
+				<StyledDiv onClick={() => logUserOut()}>Sign out</StyledDiv>
 			</Flex.Child>
 		</Flex.Container>
 	</Box>
